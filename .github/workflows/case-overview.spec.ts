3a4d09778467dbfe27170e71592c28e91e519c37
Ehab <56022276+154797@users.noreply.github.com>
2023-05-23 11:57:13 +0200

    Update case-overview.spec.ts

--- .github/workflows/case-overview.spec.ts
+++ .github/workflows/case-overview.spec.ts
@@ -1,7 +1,7 @@
-import test, { expect } from '.github/workflows/@playwright/test';
-import { Chance } from '.github/workflows/chance';
-import { ExamUtils } from '.github/workflows/page-models/test.ts';
-import { appSettings } from '.github/workflows/app-settings.ts';
+import test, { expect } from '../@playwright/test';
+import { Chance } from '../chance';
+import { ExamUtils } from '../page-models/test.ts';
+import { appSettings } from '../app-settings.ts';
 
 const chance = new Chance();
 
